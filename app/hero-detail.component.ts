abe563402c9e1cf3e324d8415bb0d58d92f5236d
BOOLRon <ron.zhang.36@gmail.com>
2016-12-28 16:25:43 +0800

    learn to `HTTP`
        https://angular.io/docs/ts/latest/tutorial/toh-pt6.html

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,33 +1,45 @@
-import { Component, OnInit} from '@angular/core';
+import 'rxjs/add/operator/switchMap';
+import { Component, OnInit }      from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { Location } from '@angular/common';
-import { Hero } from './hero';
+import { Location }               from '@angular/common';
+
+import { Hero }        from './hero';
 import { HeroService } from './hero.service';
 
-import 'rxjs/add/operator/switchMap';
-
 @Component({
-    moduleId: module.id,
-    selector: 'my-hero-detail',
-    templateUrl: 'hero-detail.component.html',
-    styleUrls: [ 'hero-detail.component.css' ]
+  moduleId: module.id,
+  selector: 'my-hero-detail',
+  templateUrl: 'hero-detail.component.html',
+  styleUrls: [ 'hero-detail.component.css' ]
 })
+export class HeroDetailComponent implements OnInit {
+  hero: Hero;
 
-export class HeroDetailComponent implements OnInit{
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 
-    hero: Hero;
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
 
-    constructor(
-        private heroService: HeroService,
-        private route: ActivatedRoute,
-        private location: Location
-    ){}
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
+  }
 
-    ngOnInit(): void {
-        // this.route.params.switchMap((params: Params) => this.heroService.getHero(+params['id'])).subscribe(hero => this.hero = hero);
-    }
-
-    goBack(): void {
-        this.location.back();
-    }
+  goBack(): void {
+    this.location.back();
+  }
 }
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
